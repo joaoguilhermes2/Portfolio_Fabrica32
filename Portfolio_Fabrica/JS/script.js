e3456be3baabdbcb5fb6e997ffc231d5e8029d78
João Guilherme Ortigosa <dry_jg@hotmail.com>
2024-10-30 08:46:30 -0400

    Portfolio Fábrica 32
    Atualização

---
+++ Portfolio_Fabrica/JS/script.js
@@ -0,0 +1,14 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+});
